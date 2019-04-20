8042dbff6b7541f741fdeeeb0da383fc0ed61873
Ali Can Canbay <lcnbyy@gmail.com>
2019-04-20 19:16:34 +0300

    Delete 01_merhaba.c

--- 01_merhaba.c
+++
@@ -1,7 +0,0 @@
-/* Ekrana "Merhaba Dunya" yazisi bastirilacak */
-#include <stdio.h> //Standard input output kutuphanesi ekleniyor
-int main()
-{
-	printf("Merhaba Dunya!\n"); //yazdirma komutu
-	return 0; //programin bittigini belirtir
-}
